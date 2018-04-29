23708a2f93b2816991cacc7c408a2fac4dba221a
Zixuan Chen <v-zixch@microsoft.com>
2018-04-29 19:04:46 +0800

    env

--- bash/env/env.sh
+++ bash/env/env.sh
@@ -1,4 +1,4 @@
-conda install pytorch-cpu torchvision-cpu -c pytorch
+conda install -y pytorch-cpu torchvision-cpu -c pytorch
 pip install spacy
 sudo /home/ubuntu/anaconda3/bin/python -m spacy download en_core_web_lg
 python bash/env/download_wordnet.py
