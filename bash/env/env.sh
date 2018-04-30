402570c2bf2bbc2cdca30d14d6fc99865a373bd8
Zixuan Chen <v-zixch@microsoft.com>
2018-04-30 14:31:31 +0800

    update

--- bash/env/env.sh
+++ bash/env/env.sh
@@ -1,5 +1,7 @@
 conda install -y pytorch-cpu torchvision-cpu -c pytorch
 pip install spacy
+pip install sparqlwrapper
+pip install rdflib
 sudo /home/ubuntu/anaconda3/bin/python -m spacy download en_core_web_lg
 python bash/env/download_wordnet.py
 
