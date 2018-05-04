d8358f1a4f489a3276a90302a9ec6e6572c24650
Zixuan Chen <v-zixch@microsoft.com>
2018-05-04 18:50:31 +0800

    imb bash

---
+++ bash/train-nouns-cos-imb-w2vsim-ontrainset.10d.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Get number of threads from environment or set to default
+if [ -z "$NTHREADS" ]; then
+   NTHREADS=5
+fi
+
+echo "Using $NTHREADS threads"
+
+# make sure OpenMP doesn't interfere with pytorch.multiprocessing
+export OMP_NUM_THREADS=1
+
+# The hyperparameter settings reproduce the mean rank results 
+# reported in [Nickel, Kiela, 2017]
+# For MAP results, replace the learning rate parameter with -lr 2.0
+
+python3 embed.py \
+       -dim 10 \
+       -lr 1.0 \
+       -epochs 800 \
+       -negs 50 \
+       -burnin 20 \
+       -nproc "${NTHREADS}" \
+       -distfn poincare \
+       -dset wordnet/noun_closure.train.tsv \
+       -dset_test wordnet/noun_closure.test.tsv \
+       -fout model/nouns.10d.cos.train \
+       -batchsize 50 \
+       -eval_each 10 \
+       -w2v_sim \
+       -mapping_func cos \
+       -nobalance \
+       -override
