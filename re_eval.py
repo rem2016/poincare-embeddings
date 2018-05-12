5427fcba4f1bb2e61bb6a84d487407b73f9f7cab
Zixuan Chen <v-zixch@microsoft.com>
2018-05-12 18:59:45 +0800

    dd

--- re_eval.py
+++ re_eval.py
@@ -23,10 +23,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 word_sim_data = WordSimDataset()
-simlex_pairs, simlex_human = word_sim_data.load_dataset('noun_ws353')
-simlex_pairs = list(simlex_pairs)
+refer_pairs, refer_human_ground_truth = word_sim_data.load_dataset('noun_rg')
+refer_pairs = list(refer_pairs)
 datasets = ['noun_rg', 'noun_mc', 'noun_ws353', 'noun_ws353-sim', 'noun_simlex']
 ground_truth_pairs = [word_sim_data.load_dataset(dataset) for dataset in datasets]
+nlp = spacy.load('en_core_web_lg')
 
 
 class LogInfo:
@@ -241,7 +242,7 @@ def _load_model(state_dict, size, dim):
 def re_rank(model_info, train_set, test_set):
     model_ = th.load(model_info.path_to_the_best_model())
     objs = model_['objects']
-    index2word = model_['word_index']
+    index2word = model_.get('word_index', None)
     emb = model_['model']
     model_ = _load_model(emb, emb['lt.weight'].size(0), emb['lt.weight'].size(1))
     idx, objects, dwords = slurp(train_set,
@@ -321,7 +322,10 @@ def eval_on_scws(model_info):
         def norm(v):
             return th.sqrt(th.sum(v**2, -1))
 
-        ret = th.sum(v1 * v2, dim=-1) / (norm(v1) * norm(v2))
+        _norm = (norm(v1) * norm(v2))
+        if _norm < 1e-10:
+            return None
+        ret = th.sum(v1 * v2, dim=-1) / _norm
         return ret.squeeze(-1)
 
     def _neg_sim(s1, s2):
@@ -387,11 +391,11 @@ def eval_on_scws(model_info):
             raise e
 
     def _fit_simlex(*sim_funcs):
-        tds = [eval_scws.Td(x, y, x, y, s) for (x, y), s in zip(simlex_pairs, simlex_human)]
+        tds = [eval_scws.Td(x, y, x, y, s) for (x, y), s in zip(refer_pairs, refer_human_ground_truth)]
         _preds = [[sim_func(td) for sim_func in sim_funcs] for td in tds]
         _X = []
         _y = []
-        for _pred, score in zip(_preds, simlex_human):
+        for _pred, score in zip(_preds, refer_human_ground_truth):
             if any((x is None for x in _pred)):
                 continue
             _X.append([float(x) for x in _pred])
@@ -405,12 +409,12 @@ def eval_on_scws(model_info):
 
     def _calc_sim_lr_simlex(td):
         a = _calc_sim_closest(td)
-        b = _calc_sim_word_dist(td)
+        b = _calc_sim_word_cos(td)
         return a is None or b is None or lr is None or lr.predict([[a, b]])
 
     def _calc_sim_lr_simlex_context(td):
         a = _calc_sim(td)
-        b = _calc_sim_word_dist(td)
+        b = _calc_sim_word_cos(td)
         return a is None or b is None or lr is None or lr.predict([[a, b]])
 
     def _word2vec_sim(td):
@@ -428,13 +432,44 @@ def eval_on_scws(model_info):
 
         def __eval(_p, _hs):
             tds = [eval_scws.Td(x, y, x, y, s) for (x, y), s in zip(_p, _hs)]
-            X = [[_calc_sim_closest(td), _calc_sim_word_dist(td)] for td in tds]
-            y = [h for _x, h in zip(X, _hs) if _x[0] is not None and _x[1] is not None]
-            X = [_x for _x in X if _x[0] is not None and _x[1] is not None]
+            X = [[_calc_sim_closest(td), _calc_sim_word_cos(td)] for td in tds]
+            x1s = [x[1] for x in X if x[1] is not None]
+            mean_x1 = sum(x1s) / len(x1s)
+            x0s = [x[0] for x in X if x[0] is not None]
+            mean_x0 = sum(x0s) / len(x0s)
+            pred = []
+            y = []
+            for _x, _h in zip(X, _hs):
+                if _x[0] is None and _x[1] is None:
+                    continue
+                y.append(float(_h))
+                if _x[1] is None:
+                    _x[1] = mean_x1
+                elif _x[0] is None:
+                    _x[0] = mean_x0
+                try:
+                    pred.append(float(lr.predict([_x])[0]))
+                except ValueError:
+                    print(_x)
+            if len(pred) == 0:
+                return None
+            return np.corrcoef(pred, y)[0, 1]
+
+        _ret = {}
+        for name, data in zip(datasets, ground_truth_pairs):
+            pairs, human = data
+            _ret[name] = __eval(pairs, human)
+        return _ret
+
+    def _calc_closest_sim():
+        def __eval(_p, _hs):
+            tds = [eval_scws.Td(x, y, x, y, s) for (x, y), s in zip(_p, _hs)]
+            X = [_calc_sim_closest(td) for td in tds]
+            y = [h for _x, h in zip(X, _hs) if _x is not None]
+            X = [_x for _x in X if _x is not None]
             if len(X) == 0:
                 return None
-            pred = lr.predict(X)
-            return np.corrcoef(pred, y)[0, 1]
+            return np.corrcoef(X, y)[0, 1]
 
         _ret = {}
         for name, data in zip(datasets, ground_truth_pairs):
@@ -444,16 +479,14 @@ def eval_on_scws(model_info):
 
     start = time.time()
     scws = _load_scws_data()
-    print(time.time() - start)
     tfidf = eval_scws.Tfidf()
     tfidf.fit()
 
-    nlp = spacy.load('en_core_web_lg')
     model_, objs, index2word, emb = model_info.load_model()
     iobjs = {name: i for i, name in enumerate(objs)}
     word2index = {name: i for i, name in enumerate(index2word)} if index2word is not None else None
-    lr = _fit_simlex(_calc_sim_closest, _calc_sim_word_dist)
-    print("Model loading finished. Start calculating")
+    lr = _fit_simlex(_calc_sim_closest, _calc_sim_word_cos)
+    print("        Model loading finished. Start calculating")
     ans = {}
     ans['no_idf_context'] = _eval(_calc_sim)
     ans['with_idf_context'] = _eval(lambda t: _calc_sim(t, use_idf=True))
@@ -462,6 +495,7 @@ def eval_on_scws(model_info):
     ans['word_dist'] = _eval(_calc_sim_word_dist)
     ans['lr_closest'] = _eval(_calc_sim_lr_simlex)
     ans['lr_context'] = _eval(_calc_sim_lr_simlex_context)
+    ans['closest_sim'] = _calc_closest_sim()
     ans['all_sim_lr'] = _calc_all_sim_lr()
     # ans['word2vec'] = _eval(_word2vec_sim)  # 0.64454779 1323/1328
     return ans
@@ -500,6 +534,7 @@ def re_eval_model_scws(path, name=''):
     print(path)
     start = time.time()
     model_info = ModelInfo(path)
+    print(model_info.path_to_the_best_model().split('\\')[-1])
     for k, v in eval_on_scws(model_info).items():
         print('- {: <22}{: <22}'.format(str(k), str(v)))
     print('Used time', time.time() - start)
@@ -507,12 +542,16 @@ def re_eval_model_scws(path, name=''):
 
 
 if __name__ == '__main__':
-    models = load_all_important_models()
-    for _model in models:
-        try:
-            re_eval_model_scws(_model)
-        except Exception as e:
-            raise e
+    re_rank_model(r"C:\Users\Administrator\Documents\G\model\data\PURE10d")
+    re_eval_model_scws(r"C:\Users\Administrator\Documents\G\model\data\PURE10d")
+    # re_rank_model(r'C:\Users\Administrator\Documents\G\model\data\80D512\80D_w2vsim_imb.lr=1.0.dim=80.negs=50.burnin=20.batch=50')
+    # re_eval_model_scws(r'C:\Users\Administrator\Documents\G\model\data\80D512\80D_w2vsim_imb.lr=1.0.dim=80.negs=50.burnin=20.batch=50')
+    # models = load_all_important_models()
+    # for _model in models:
+    #     try:
+    #         re_eval_model_scws(_model)
+    #     except Exception as e:
+    #         raise e
     # re_eval_model_scws(r'C:\Users\Administrator\Documents\G\model\data\LatestHuge'
     #                   r'\GRADUALLY.50d.cos.train_w2vsim_cos_imb.lr=1.0.dim=50.negs=50.burnin=20.batch=50',
     #                   'Gradually burnin')
@@ -522,3 +561,96 @@ if __name__ == '__main__':
     # re_eval_model_scws(r'C:\Users\Administrator\Documents\G\model\data\LatestSm'
     #                   r'\nouns.50d.train.lr=1.0.dim=50.negs=50.burnin=20.batch=50',
     #                   'Naive 50d')
+
+
+r"""
+
+=============================================
+
+      Start: Pure 10d
+
+=============================================
+C:\Users\Administrator\Documents\G\model\data\PURE10d
+Loading wordnet words
+Total 37539 words are added
+Loaded all_nn.tsv. Words:  37539, num: 578053, average: 15.398731985401849
+slurp: objects=82115, edges=767086, words=37539
+
+Start ranking train set
+Start ranking test set
+{'train': (6.23669568632585, 0.5353748937148014), 'test': (5.518334745249909, 0.5210224501164599)}
+Used time 523.3364696502686
+======================================
+=============================================
+
+      Start: Pure 10d
+
+=============================================
+C:\Users\Administrator\Documents\G\model\data\PURE10d
+824.nth
+        Model loading finished. Start calculating
+        Sims num 1299/1328
+        Sims num 1328/1328
+C:\Users\Administrator\Anaconda3\lib\site-packages\numpy\lib\function_base.py:3183: RuntimeWarning: invalid value encountered in true_divide
+  c /= stddev[:, None]
+C:\Users\Administrator\Anaconda3\lib\site-packages\numpy\lib\function_base.py:3184: RuntimeWarning: invalid value encountered in true_divide
+  c /= stddev[None, :]
+- no_idf_context        None                  
+- with_idf_context      None                  
+- closest               0.45485046032399784   
+- word_cos              None                  
+- word_dist             None                  
+- lr_closest            nan                   
+- lr_context            None                  
+- closest_sim           {'noun_rg': 0.7950109570207747, 'noun_mc': 0.7856466620769119, 'noun_ws353': 0.30704477983325423, 'noun_ws353-sim': 0.5810733842950273, 'noun_simlex': 0.5544896524660374}
+- all_sim_lr            None                  
+Used time 6.792054653167725
+======================================
+
+
+=============================================
+
+      Start: 80d gradually
+
+=============================================
+C:\Users\Administrator\Documents\G\model\data\80D512\80D_w2vsim_imb.lr=1.0.dim=80.negs=50.burnin=20.batch=50
+Loading wordnet words
+Total 37422 words are added
+Loaded all_nn.tsv. Words:  37422, num: 575073, average: 15.367243867243868
+slurp: objects=81690, edges=766765, words=37422
+[ERROR]: Total 24 nodes weren't found in the train set
+
+Start ranking train set
+Start ranking test set
+{'train': (27.496974822718105, 0.5796454589927496), 'test': (27.889952733002062, 0.5290506835705141)}
+Used time 1076.9975526332855
+======================================
+=============================================
+
+      Start: 80d gradually
+
+=============================================
+C:\Users\Administrator\Documents\G\model\data\80D512\80D_w2vsim_imb.lr=1.0.dim=80.negs=50.burnin=20.batch=50
+199.nth
+Fit simlex, final corr with simlex = 0.7694646909933712
+        Model loading finished. Start calculating
+        Sims num 1298/1328
+        Sims num 1298/1328
+        Sims num 1298/1328
+        Sims num 1256/1328
+        Sims num 1256/1328
+        Sims num 1328/1328
+        Sims num 1328/1328
+- no_idf_context        0.380920090228718     
+- with_idf_context      0.37051007360327554   
+- closest               0.39002089875828544   
+- word_cos              0.3758685372651869    
+- word_dist             0.44063017797257786   
+- lr_closest            0.427079433517945     
+- lr_context            0.42279512829325977   
+- closest_sim           {'noun_rg': 0.7031826226367452, 'noun_mc': 0.6867344709022339, 'noun_ws353': 0.24921007970810885, 'noun_ws353-sim': 0.5076110117558662, 'noun_simlex': 0.4612737021659282}
+- all_sim_lr            {'noun_rg': 0.7694646909933712, 'noun_mc': 0.7471274785884376, 'noun_ws353': 0.2782686730163062, 'noun_ws353-sim': 0.5790531576930787, 'noun_simlex': 0.48519162734707605}
+Used time 10.489257574081421
+======================================
+
+"""
